333390c082c737e81f02d78c80263f8548e485df
oxfordblucher <hiuchanhk@gmail.com>
2020-11-04 18:34:18 -0500

    working render

--- app.js
+++ app.js
@@ -9,7 +9,99 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { type } = require("os");
+const Employee = require("./lib/Employee");
 
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your employee's name?"
+    },
+    {
+        type: "number",
+        name: "id",
+        message: "What is their employee ID#?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is their role?",
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type: "number",
+        name: "officeNumber",
+        message: "Which office do they manage?",
+        when: (answers) => answers.role === 'Manager'
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school do they attend?",
+        when: (answers) => answers.role === 'Intern'
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is their github?",
+        when: (answers) => answers.role === 'Engineer'
+    },
+    {
+        type: "confirm",
+        name: "more",
+        message: "Enter another employee?"
+    }
+]
+
+const employeeList = []
+let teamMembers = []
+
+async function addEmployee() {
+    await inquirer.prompt(questions).then(function(data) {
+        const employeeInfo = {
+            name: data.name,
+            id: data.id,
+            email: data.email,
+            role: data.role,
+            github: data.github,
+            office: data.officeNumber,
+            school: data.school
+        }
+        employeeList.push(employeeInfo)
+        if (data.more) {
+            addEmployee();
+        } else {
+            for (let i = 0; i < employeeList.length; i++) {
+                let employee = employeeList[i]
+
+                switch (employee.role) {
+                    case "Manager":
+                        const manager = new Manager (employee.name, parseInt(employee.id), employee.email, parseInt(employee.office))
+                        teamMembers.push(manager)
+                        break;
+                    case "Engineer":
+                        const engineer = new Engineer (employee.name, parseInt(employee.id), employee.email, employee.github)
+                        teamMembers.push(engineer)
+                        break;
+                    case "Intern":
+                        const intern = new Intern (employee.name, parseInt(employee.id), employee.email, employee.school)
+                        teamMembers.push(intern)
+                        break;
+                }
+            }
+            let teamHTML = render(teamMembers);
+            fs.writeFileSync(outputPath, teamHTML);
+        }
+    })
+}
+
+addEmployee();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
